a3fb086ee05bcbc08242c9837558078745d4a4c0
franciscoCorralesCastro2394 <47618218+franciscoCorralesCastro2394@users.noreply.github.com>
2020-04-22 08:24:14 -0600

    cambios en repo

---
+++ AUT_EndToEnd_App/App_End_to_End.TestU/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_End_to_End.TestU
+{
+    public class Class1
+    {
+    }
+}
